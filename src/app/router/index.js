b99f5b59a084ee0509acee7d29c8f3592085ff99
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-05-22 14:56:34 +0300

    feat(custom): added close menu when change route

--- src/app/router/index.js
+++ src/app/router/index.js
@@ -1,9 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import routes from "./routes";
 
+import { useMenuStore } from "@/modules/Menu";
+
 const router = createRouter({
   history: createWebHashHistory("/VueNews/"),
   routes,
+});
+
+router.beforeEach(() => {
+  const menuStore = useMenuStore();
+
+  menuStore.closeMenu();
 });
 
 export default router;
