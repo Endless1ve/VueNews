c9d272380cf61e0b836d0671a3c3cf1df3e55f3f
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-05-22 12:17:25 +0300

    feat(custom): added search ui components

---
+++ src/shared/ui/inputs/searchInput.vue
@@ -0,0 +1,39 @@
+<script setup>
+  const $props = defineProps({
+    placeholder: {
+      type: String,
+      required: true,
+    },
+  });
+  const model = defineModel();
+</script>
+
+<template>
+  <input
+    type="text"
+    :placeholder="$props.placeholder"
+    v-model="model"
+    class="searchInput" />
+</template>
+
+<style lang="scss" scoped>
+  .searchInput {
+    width: 100%;
+    border: 1px $second-ligth-color solid;
+    border-radius: 100px;
+    padding: 20px 208px 20px 20px;
+    outline: none;
+    font-family: $text-font;
+    font-size: 16px;
+    line-height: 137%;
+    background-color: $main-light-color;
+    stroke-width: 1px;
+
+    @media screen and (max-width: 768px) {
+      padding: 20px 196px 20px 20px;
+    }
+    @media screen and (max-width: 425px) {
+      padding: 17px;
+    }
+  }
+</style>
