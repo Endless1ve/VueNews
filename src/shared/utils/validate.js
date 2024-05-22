25ce8c4fda30571d915e0193137ed2ff06925fde
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-05-22 12:59:19 +0300

    feat(custom): added validation util

---
+++ src/shared/utils/validate.js
@@ -0,0 +1,22 @@
+import useVuelidate from "@vuelidate/core";
+import { helpers, required } from "@vuelidate/validators";
+
+const searchRules = {
+  query: {
+    required: helpers.withMessage("Поле не должно быть пустым", required),
+  },
+};
+
+function setupValidation(rules, fields) {
+  return useVuelidate(rules, fields);
+}
+
+function validate(v$, callback) {
+  v$.value.touch();
+  if (v$.value.$invalid) {
+    callback();
+    v$.value.reset();
+  }
+}
+
+export { setupValidation, validate, searchRules };
